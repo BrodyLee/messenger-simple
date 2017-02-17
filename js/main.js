f08c031797f19836ebefdd680e9703a8e1e5d490
Anatoly Kholozyon <kholozyon@sandglass.com.ua>
2017-02-17 21:22:00 +0200

    1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Lee on 21.12.2016.
+ */
+$(document).ready(function() {
+    $('.modal').modal();
+    $('select').material_select();
+    $('.dropdown-button').dropdown('open');
+});
